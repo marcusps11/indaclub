fffbe4c42566b0888328b708e66fbcdeee26d61a
marcusps11 <marcus@1roof.com>
2015-08-05 16:18:46 +0100

    styling

--- indaclub/app/controllers/users_controller.rb
+++ indaclub/app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :edit, :update, :destroy, :update]
+  before_action :set_user, only: [:show, :edit, :update, :destroy, :update, :upvote]
 
   # GET /users
   def index
